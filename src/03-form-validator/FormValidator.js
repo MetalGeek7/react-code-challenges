3a4484da5cc5d6ef448ee190ed980df363f51a85
AbhishekKanchan <abhishek.absmw@gmail.com>
2023-01-10 17:58:15 -0600

    Ch03 - FormValidator soln. * Updated README

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,45 +1,52 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 
-export default function FormValidator () {
+export default function FormValidator() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
-  const isValidForm = useState('')
+  const [isValidForm, setValidForm] = useState('')
 
   const submit = (e) => {
     e.preventDefault()
-    let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     
+    let regexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
     const errors = []
+    setValidForm('')
     if (!email || !password || !passwordConfirm) {
       errors.push('All fields must be filled in')
     }
     if (!email.match(regexp)) {
-      errors.push("An email must have exactly one @ sign.")    
+      errors.push("Invalid email entered 😢")
     }
     if (password.length < 8) {
       errors.push('Password length must be greater than or equal to 8 chars')
     }
-
-    if (password != passwordConfirm) {
+    if (password !== passwordConfirm) {
       errors.push('Password and confirm password field must match')
     }
 
-    
+    if (errors.length > 0) {
+      console.log(`Errors found while Form validation = ${errors}`)
+      setValidForm(errors.join(', '))
+    } else {
+      console.log('No error found while form validation.')
+      setValidForm('Howdy User! Your account has been created successfully 😎')
+    }
     setEmail("");
     setPassword("")
+    setPasswordConfirm("")
+    document.getElementById("sign_up").reset();
   };
 
   return (
-    <form onSubmit={submit}>
+    <form id="sign_up" onSubmit={submit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
         type='text' name='email'
         onChange={e => {
           setEmail(e.target.value)
-          // console.log(`${email}`)
         }}
       />
       <label htmlFor='password'>Password</label>
@@ -53,7 +60,7 @@ export default function FormValidator () {
         onChange={e => setPasswordConfirm(e.target.value)}
       />
       <input type='submit' value='Submit' />
-      {"User will be created...."}
+      {isValidForm}
     </form>
   )
 }
